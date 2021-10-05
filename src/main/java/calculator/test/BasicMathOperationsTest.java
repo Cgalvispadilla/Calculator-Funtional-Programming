02da7397be4f34a23d4387367b5f929d6767d507
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-10-05 18:17:04 -0500

    test de suma y division realizado

--- src/main/java/calculator/test/BasicMathOperationsTest.java
+++ src/main/java/calculator/test/BasicMathOperationsTest.java
@@ -9,6 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BasicMathOperationsTest {
     @Test
+    @DisplayName("esta prueba valida el funcionamiento de la suma")
+    void validarFuncionamientoDeSuma(){
+        //assert
+        double numberOne=15;
+        double numberTwo=30;
+        double result=45;
+
+        //act
+        var res= sum(numberOne,numberTwo);
+
+        //assert
+        Assertions.assertEquals(result,res);
+    }
+    @Test
     @DisplayName("esta prueba valida el funcionamiento de la resta")
     void validarFuncionamientoDeResta(){
         //assert
@@ -50,5 +64,46 @@ class BasicMathOperationsTest {
         //assert
         Assertions.assertEquals(result,res);
     }
+    @Test
+    @DisplayName("esta prueba valida el funcionamiento correcto de la división entera")
+    void validarElFuncionamientoCorrectoDeLaDivisionEntera(){
+        //assert
+        double dividend=25;
+        double divider=5;
+        double result=5;
 
+        //act
+        var res= division(dividend,divider);
+
+        //assert
+        Assertions.assertEquals(result,res);
+    }
+    @Test
+    @DisplayName("esta prueba valida el funcionamiento correcto de la división por cero")
+    void validarElFuncionamientoCorrectoDeLaDivisionPorCero(){
+        //assert
+        double dividend=5;
+        double divider=0;
+        double result=-1;
+
+        //act
+        var res= division(dividend,divider);
+
+        //assert
+        Assertions.assertEquals(result,res);
+    }
+    @Test
+    @DisplayName("esta prueba valida el funcionamiento correcto de la división en un número negativo")
+    void validarElFuncionamientoCorrectoDeLaDivisionEnNumeroNegativo(){
+        //assert
+        double dividend=25;
+        double divider=-5;
+        double result=-1;
+
+        //act
+        var res= division(dividend,divider);
+
+        //assert
+        Assertions.assertEquals(result,res);
+    }
 }
