e13371d96d4c37f4f1cd6de18e7cf14c50a84d4c
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-10-05 11:47:51 -0500

    project start-up

---
+++ src/main/java/calculator/main.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class main {
+    public static void main(String[] args) {
+
+        double numero1 = 1;
+        double numero2 = 2;
+
+        public double sumar(numero1,numero2){
+            return numero1+ numero2;
+        }
+
+
+
+    }
+}
